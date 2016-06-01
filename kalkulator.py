7acd64c6856ecfcba3f14ba296e32cbdd00656a3
radoslawwalczak <radoslaw.walczak@wp.pl>
2016-06-01 09:54:55 +0200

    dodano pkil kalkulator.py

---
+++ kalkulator.py
@@ -0,0 +1,14 @@
+#-------------------------------------------------------------------------------
+# Name:        kalkulator
+# Purpose:
+#
+# Author:      radek
+#
+# Created:     01-06-2016
+# Copyright:   (c) radoslaw.walczak@wp.pl 2016
+#-------------------------------------------------------------------------------
+
+def dodawanie(a:float,b:float) -> float:
+    return a+b
+
+print( dodawanie(2,2) )
